4e5417a036b1a8b2688595a04c329702008dffa8
maragramonte <agramontemar@gmail.com>
2024-10-25 09:27:07 +0200

    Add files via upload

---
+++ Calculo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Calculo{
+  public static void main(String[]args){
+  int max1;
+  int max2;
+  System.out.prinIn(""El numero max es=""max(1,2));
+
+  if(1>=2){
+    return 1;
+  }else
+    return 2;
+  }
+}
